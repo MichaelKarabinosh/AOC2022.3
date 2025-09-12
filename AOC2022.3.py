47d88d47350ec1941a274b1794a23c7ac580ffb6
MichaelKarabinosh <michaelk368@nycstudents.net>
2025-09-12 10:19:52 -0400

    part 1 complete

--- AOC2022.3.py
+++ AOC2022.3.py
@@ -2,10 +2,20 @@ with open("InputFile", "r") as file:
     lines = file.readlines()
     num = 0
     for line in lines:
-        half = int ((len(line) / 2))
+        half = int(len(line) / 2)
         firstComp = line[:half]
         secondComp = line[half:]
         for string in firstComp:
-           if string in secondComp:
-               num += ord(string) - ord('A')
-               print(num)
+            if string in secondComp:
+                if string.isupper():
+                    num += ord(string.lower()) - 70
+                else:
+                    num += ord(string) - 96
+                break
+
+    print("Part One:", num)
+
+
+
+
+
