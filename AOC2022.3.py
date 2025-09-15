b5e692f183e001c30f8a9ef7cadb04df87c3c97c
MichaelKarabinosh <michaelk368@nycstudents.net>
2025-09-15 10:19:31 -0400

    part 2 progress

--- AOC2022.3.py
+++ AOC2022.3.py
@@ -14,6 +14,31 @@ with open("InputFile", "r") as file:
                 break
 
     print("Part One:", num)
+    num = 0
+    counter = 0
+    res = []
+    n = 3
+
+    for i in range(0,len(lines),n):
+       res.append(lines[i:i + n])
+
+    for entry in res:
+        firstComp = res[0]
+        secondComp = res[1]
+        thirdComp = res[3]
+        for string in firstComp:
+            if string in secondComp:
+                if string in thirdComp:
+                    if string != "\n":
+                        if string.isupper():
+                            num += ord(string.lower()) - 70
+                        else:
+                            num += ord(string) - 96
+                        break
+    print(num)
+    print(res)
+    hi = "jkff"
+
 
 
 
